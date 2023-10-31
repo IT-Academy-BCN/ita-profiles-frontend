a33a7cdf48f0931f39025c15ebb218011a82e254
AlbertLnz <albert2000_lanza@hotmail.com>
2023-10-31 14:32:53 +0100

    React Redux & Axios installed

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,8 @@ import './App.css'
 function App() {
   return (
     <>
-<div tabIndex={0} className="collapse bg-base-200"> 
-  <div className="collapse-title text-xl font-medium">
-    Focus me to see content
-  </div>
-  <div className="collapse-content"> 
-    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
-  </div>
-</div>    </>
+      <p>hola mundo!</p>
+    </>
   )
 }
 

0919ab827d9c69051fd6f6381b47d9d5495719a4
llopFilms <llopFilms@gmail.com>
2023-11-02 19:59:36 +0100

    Resolved eslint error referred to 'require' daisyui plugin. Changed to 'import' syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: "class",
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-}
+  plugins: [daisyui],
+};

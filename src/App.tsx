e7ab5ef42c91a8a3ef03f4110d6b0a7b9622eb82
Dar-ya218 <dasharoura13465@gmail.com>
2023-11-07 11:50:54 +0100

    lock popup works

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import LoginButton from "./components/LoginButton";
+import RestrictedAccessPopup from "./components/RestrictedAccessPopup";
 import './styles/App.css'
 
 const App: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   function handleClick(): void {
-    throw new Error("Function not implemented.");
+    setIsModalOpen(true);
+  }
+
+  function handleClose(): void {
+    setIsModalOpen(false);
   }
 
   return (
     <>
      <h1>Welcome to ita profiles</h1>
-      <LoginButton onClick={handleClick}/>
+      <LoginButton onClick={handleClick} />
+      {isModalOpen && <RestrictedAccessPopup onClose={handleClose} />}
     </>
-  )
-}
-
+  );
+};
 export default App
--- src/components/RestrictedAccessPopup.tsx
+++ src/components/RestrictedAccessPopup.tsx
@@ -1,79 +1,98 @@
 import React from "react";
+import Lock from '../assets/svg/lock-dynamic-color.svg';
 
-const RestrictedAccessPopup: React.FC = () => {
+type ModalProps = {
+  onClose: () => void;
+};
+
+const RestrictedAccessPopup: React.FC<ModalProps> = ({ onClose }) => {
   return (
     <div
       style={{
-        width: "719px",
-        height: "547px",
-        top: "174px",
-        left: "349px",
-        borderRadius: "12px",
-        position: "absolute",
-        backgroundColor: "white",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 9999,
       }}
     >
-      <img
-        src="lock-image.png"
-        alt="Lock"
-        style={{
-          width: "118px",
-          height: "118px",
-          top: "250px",
-          left: "650px",
-          position: "absolute",
-        }}
-      />
-      <h2
-        style={{
-          textAlign: "center",
-          marginTop: "400px",
-          fontSize: "24px",
-        }}
-      >
-        Acceso restringido
-      </h2>
-      <p
-        style={{
-          textAlign: "center",
-          fontSize: "18px",
-        }}
-      >
-        Entra o regístrate
-      </p>
       <div
         style={{
+          width: "300px",
+          borderRadius: "12px",
+          backgroundColor: "white",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          marginTop: "20px",
+          padding: "20px",
+          position: "relative",
         }}
       >
         <button
           style={{
-            width: "309px",
-            height: "63px",
-            borderRadius: "12px",
-            backgroundColor: "#B91879",
-            color: "white",
-            fontSize: "18px",
-            marginBottom: "10px",
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            width: "30px",
+            height: "30px",
+            borderRadius: "50%",
+            backgroundColor: "transparent",
+            border: "none",
+            cursor: "pointer",
           }}
+          onClick={onClose}
         >
-          Soy candidato
+          ✕
         </button>
-        <button
+        <img
+          src={Lock}
+          alt="Lock"
           style={{
-            width: "309px",
-            height: "63px",
-            borderRadius: "12px",
-            backgroundColor: "#B91879",
-            color: "white",
-            fontSize: "18px",
+            width: "118px",
+            height: "118px",
+            marginBottom: "20px",
           }}
-        >
-          Soy reclutador
-        </button>
+        />
+        <h2 style={{ fontSize: "24px", marginBottom: "10px" }}>
+          Acceso restringido
+        </h2>
+        <p style={{ fontSize: "18px", marginBottom: "30px" }}>
+          Entra o regístrate
+        </p>
+        <div>
+          <button
+            style={{
+              width: "100%",
+              height: "63px",
+              borderRadius: "12px",
+              backgroundColor: "#B91879",
+              color: "white",
+              fontSize: "18px",
+              marginBottom: "10px",
+            }}
+            onClick={onClose}
+          >
+            Soy candidato/a
+          </button>
+          <button
+            style={{
+              width: "100%",
+              height: "63px",
+              borderRadius: "12px",
+              backgroundColor: "#B91879",
+              color: "white",
+              fontSize: "18px",
+            }}
+            onClick={onClose}
+          >
+            Soy reclutador/a
+          </button>
+        </div>
       </div>
     </div>
   );

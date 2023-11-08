717f297eb30ff2188954185c3fa407f6365fb28d
Dar-ya218 <dasharoura13465@gmail.com>
2023-11-09 00:51:02 +0100

    second popup built, like in Figma

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,144 @@
-import React, {useState} from "react";
-import LoginButton from "./components/LoginButton";
-import RestrictedAccessPopup from "./components/RestrictedAccessPopup";
-import './styles/App.css'
+import React, { useState } from "react";
+import Lock from './assets/svg/lock-dynamic-color.svg';
+import RegisterPopup from "./components/RegisterPopup";
 
 const App: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  function handleClick(): void {
-    setIsModalOpen(true);
-  }
+  const [isRestrictedPopupOpen, setIsRestrictedPopupOpen] = useState(false);
+  const [isRegisterPopupOpen, setIsRegisterPopupOpen] = useState(false);
 
-  function handleClose(): void {
-    setIsModalOpen(false);
-  }
+  const handleOpenRestrictedPopup = () => {
+    setIsRestrictedPopupOpen(true);
+  };
+
+  const handleCloseRestrictedPopup = () => {
+    setIsRestrictedPopupOpen(false);
+  };
+
+  const handleOpenRegisterPopup = () => {
+    setIsRegisterPopupOpen(true);
+    setIsRestrictedPopupOpen(false); // Cerrar el primer popup al abrir el segundo
+  };
+
+  const handleCloseRegisterPopup = () => {
+    setIsRegisterPopupOpen(false);
+  };
 
   return (
-    <>
-     <h1>Welcome to ita profiles</h1>
-      <LoginButton onClick={handleClick} />
-      {isModalOpen && <RestrictedAccessPopup onClose={handleClose} />}
-    </>
+    <div>
+      <h1>Welcome to ita profiles</h1>
+      <button onClick={handleOpenRestrictedPopup}>Entrar</button>
+
+      {isRestrictedPopupOpen && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 9999,
+          }}
+        >
+          <div
+            style={{
+              width: "300px",
+              borderRadius: "12px",
+              backgroundColor: "white",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: "20px",
+              position: "relative",
+            }}
+          >
+            <button
+              style={{
+                position: "absolute",
+                top: "10px",
+                right: "10px",
+                width: "30px",
+                height: "30px",
+                borderRadius: "50%",
+                backgroundColor: "transparent",
+                border: "none",
+                cursor: "pointer",
+              }}
+              onClick={handleCloseRestrictedPopup}
+            >
+              ✕
+            </button>
+            <img
+              src={Lock}
+              alt="Lock"
+              style={{
+                width: "118px",
+                height: "118px",
+                marginBottom: "20px",
+              }}
+            />
+            <h2 style={{ fontSize: "24px", marginBottom: "10px" }}>
+              Acceso restringido
+            </h2>
+            <p style={{ fontSize: "18px", marginBottom: "30px" }}>
+              Entra o regístrate para acceder al perfil
+            </p>
+            <div>
+              <button
+                style={{
+                  width: "100%",
+                  height: "63px",
+                  borderRadius: "12px",
+                  backgroundColor: "#B91879",
+                  color: "white",
+                  fontSize: "18px",
+                  marginBottom: "10px",
+                }}
+                onClick={handleOpenRegisterPopup}
+              >
+                Soy candidato/a
+              </button>
+              <button
+                style={{
+                  width: "100%",
+                  height: "63px",
+                  borderRadius: "12px",
+                  backgroundColor: "#B91879",
+                  color: "white",
+                  fontSize: "18px",
+                }}
+                onClick={handleCloseRestrictedPopup}
+              >
+                Soy reclutador/a
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {isRegisterPopupOpen && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 9999,
+          }}
+        >
+          <RegisterPopup onClose={handleCloseRegisterPopup} />
+        </div>
+      )}
+    </div>
   );
 };
-export default App
+
+export default App;
--- src/components/RegisterPopup.tsx
+++ src/components/RegisterPopup.tsx
@@ -11,78 +11,91 @@ const RegisterPopup: React.FC<RegisterPopupProps> = ({ onClose }) => {
   };
 
   return (
-    <div className="relative w-600 rounded-lg bg-white flex flex-col items-center p-20">
-      <h2>Registration</h2>
-      <form>
+    <div className="relative w-120 rounded-lg bg-white flex flex-col items-center p-5 md:p-20">
+      <h2 className="text-lg md:text-2xl font-bold">Registro</h2>
+      <form className="flex flex-col space-y-4">
         <button
-          className="absolute top-10 right-10 w-30 h-30 rounded-full bg-transparent border-none cursor-pointer"
+          className="absolute top-2 right-2 w-8 h-8 rounded-full bg-transparent border-none cursor-pointer"
           onClick={onClose}
         >
           ✕
         </button>
-        <div className="flex justify-between">
-          <div className="mr-20">
+        <div className="grid grid-cols-2 gap-4">
+          <div>
             <input
               type="text"
               id="dni"
-              className="w-313.563 h-61 p-4.563 px-6.563 py-5 px-7.563 justify-end items-center flex-shrink-0 rounded-12 border border-gray-600"
-              placeholder=" DNI"
+              className="w-full p-2 md:p-4 px-4 md:px-6 py-4 md:py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
+              placeholder="DNI o NIE"
             />
           </div>
           <div>
             <input
               type="text"
               id="username"
-              className="w-313.563 h-61 p-4.563 px-6.563 py-5 px-7.563 justify-end items-center flex-shrink-0 rounded-12 border border-gray-600"
-              placeholder=" Username"
+              className="w-full p-2 md:p-4 px-4 md:px-6 py-4 md:py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
+              placeholder="Username"
             />
           </div>
         </div>
-        <div className="flex justify-between">
-          <div className="mr-20">
+        <div className="grid grid-cols-2 gap-4">
+          <div>
             <input
               type="password"
               id="password"
-              className="w-313.563 h-61 p-4.563 px-6.563 py-5 px-7.563 justify-end items-center flex-shrink-0 rounded-12 border border-gray-600"
-              placeholder=" Password"
+              className="w-full p-2 md:p-4 px-4 md:px-6 py-4 md:py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
+              placeholder="Password"
             />
           </div>
           <div>
             <input
               type="email"
               id="email"
-              className="w-313.563 h-61 p-4.563 px-6.563 py-5 px-7.563 justify-end items-center flex-shrink-0 rounded-12 border border-gray-600"
-              placeholder=" Email"
+              className="w-full p-2 md:p-4 px-4 md:px-6 py-4 md:py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
+              placeholder="Email"
             />
           </div>
         </div>
-        <div className="flex justify-between">
-          <div className="mr-20">
+        <div className="grid grid-cols-2 gap-4">
+          <div>
             <input
               type="text"
               id="specialization"
-              className="w-313.563 h-61 p-4.563 px-6.563 py-5 px-7.563 justify-end items-center flex-shrink-0 rounded-12 border border-gray-600"
-              placeholder=" Specialization"
+              className="w-full p-2 md:p-4 px-4 md:px-6 py-4 md:py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
+              placeholder="Specialization"
             />
           </div>
           <div>
             <input
               type="password"
               id="confirmPassword"
-              className="w-313.563 h-61 p-4.563 px-6.563 py-5 px-7.563 justify-end items-center flex-shrink-0 rounded-12 border border-gray-600"
-              placeholder=" Confirm Password"
+              className="w-full p-2 md:p-4 px-4 md:px-6 py-4 md:py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
+              placeholder="Confirm Password"
             />
           </div>
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-centr items-center space-x-8 p-4 md:p-5 ">
+          <input type="checkbox" id="acceptTerms" className="w-6 h-6" />
+          <label htmlFor="acceptTerms" className="text-sm">
+            Acepto <span style={{ textDecoration: 'underline' }}>términos legales</span>
+          </label>
           <button
-            className="w-200 h-40 rounded-8 bg-purple-900 text-white text-base border-none cursor-pointer"
+            className="w-102 mr-6 md:w-60 h-12 md:h-12 rounded-lg bg-pink-500 text-white text-base md:text-lg border-none cursor-pointer"
             onClick={handleRegister}
           >
             Register
           </button>
         </div>
       </form>
+      <div className="text-center mt-4">
+        <a
+          href="#"
+          className="text-black font-bold cursor-pointer"
+          style={{ textDecoration: 'underline' }}
+        >
+          ¿Tienes cuenta? acceder
+        </a>
+      </div>
     </div>
   );
 };

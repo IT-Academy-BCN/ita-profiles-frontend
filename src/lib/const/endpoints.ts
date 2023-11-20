fa70e96477a0b602e44bce4654151294e679caf5
llopFilms <llopFilms@gmail.com>
2023-11-20 18:45:52 +0100

    added 'HttpBackend' to the project: - set 'localPath' as a local backend route - created 'ENDPOINTS' constant object

---
+++ src/lib/const/endpoints.ts
@@ -0,0 +1,7 @@
+export const ENDPOINTS = {
+  translation: {
+    localPath: 'src/translation/languages/{{lng}}.json',
+  },
+};
+
+export const localPath = ENDPOINTS.translation.localPath;
--- src/translation/i18n.ts
+++ src/translation/i18n.ts
@@ -1,34 +1,28 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import { ca, en,es } from './languages';
+import HttpBackend, { HttpBackendOptions } from 'i18next-http-backend';
 
 i18next
   .use(initReactI18next)
   .use(LanguageDetector)
-  .init({
+  .use(HttpBackend)
+  .init<HttpBackendOptions>({
+    backend: {
+      loadPath: 'src/translation/languages/{{lng}}.json',
+    },
     interpolation: { escapeValue: false },
     fallbackLng: 'ca',
     detection: {
       order: [
-        'htmlTag',
         'cookie',
+        'htmlTag',
         'localStorage',
         'navigator',
         'path',
         'subdomain',
       ],
-    },
-    resources: {
-      ca: {
-        translation: ca,
-      },
-      en: {
-        translation: en,
-      },
-      es: {
-        translation: es,
-      },
+      caches: ['cookie'],
     },
   });
 

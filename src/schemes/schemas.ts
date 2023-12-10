82ef1c3487f1bf74eb85d8781bf33a1badea8856
Dar-ya218 <dasharoura13465@gmail.com>
2023-12-10 13:52:22 +0100

    validation with hook forms

---
+++ src/schemes/schemas.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const UserSchema = z.object({
+  username: z.string(),
+  dni: z.string().refine((dni) => /^[0-9]{8}[A-Za-z]$/.test(dni), {
+    message: 'El DNI debe tener 8 dígitos seguidos de una letra.',
+  }),
+  email: z.string().email({ message: 'Este email no es válido.' }),
+  specialization: z.string(),
+  password: z
+    .string()
+    .min(8, { message: 'La contraseña debe tener al menos 8 caracteres.' }),
+    confirmPassword: z.string().min(8, {message: 'La contraseña debe tener al menos 8 caracteres.'})
+  }).refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword']
+});

4957c1d924dbfe8007a20c6a8b6277c900f1434f
Dar-ya218 <dasharoura13465@gmail.com>
2023-12-19 21:18:28 +0100

    fix errors

--- src/components/ProfilesList.tsx
+++ src/components/ProfilesList.tsx
@@ -1,12 +1,12 @@
-import profilesData from '../data/Profiles_JESON';
+import profilesData from '../data/profiles.json';
 import ProfileCard from './ProfileCard';
 
 const ProfilesList = () => {
   return (
     <div className="container mx-auto mt-8">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {profilesData.map((profile, index) => (
-          <ProfileCard key={index} {...profile} />
+        {profilesData.map((profile) => (
+          <ProfileCard key={profile.id} {...profile} />
         ))}
       </div>
     </div>

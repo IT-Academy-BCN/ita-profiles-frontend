5a47efc131f1659cbd3c8e7478723a70c3c3de3e
Dar-ya218 <dasharoura13465@gmail.com>
2024-01-09 13:40:16 +0100

    finish list of filters

--- src/components/filters/Filters.tsx
+++ src/components/filters/Filters.tsx
@@ -2,72 +2,82 @@ const Filters = () => {
   return (
     <div>
       <h3 className="w-40 text-2xl font-bold text-colortext">Filtros</h3>
-      <div className="font-bold">
-        <h4>Roles</h4>
+      <div>
+        <h4 className="font-bold">Roles</h4>
       <label className="cursor-pointer label">
             <input
               type="checkbox"
               className="checkbox"
             />
+          <span>Frontend</span>
           </label>
           <label className="cursor-pointer label">
             <input
               type="checkbox"
               className="checkbox"
             />
+            <span>Backend</span>
           </label>
           <label className="cursor-pointer label">
             <input
               type="checkbox"
               className="checkbox"
             />
+            <span>Fullstack</span>
           </label>
           <label className="cursor-pointer label">
             <input
               type="checkbox"
               className="checkbox"
             />
+            <span>Data Science</span>
           </label>
           </div>
 
-          <div className="font-bold">
-        <h4>Desarollo</h4>
+          <div >
+        <h4 className="font-bold">Desarollo</h4>
       <label className="cursor-pointer label">
             <input
               type="checkbox"
               className="checkbox"
             />
+            <span>Spring</span>
           </label>
           <label className="cursor-pointer label">
             <input
               type="checkbox"
               className="checkbox"
             />
+            <span>Laravel</span>
           </label>
           <label className="cursor-pointer label">
             <input
               type="checkbox"
               className="checkbox"
             />
+            <span>Angular</span>
           </label>
           <label className="cursor-pointer label">
             <input
               type="checkbox"
               className="checkbox"
             />
+            <span>React</span>
           </label>
           </div>
-          <div className="font-bold">
-            <h4>Estados</h4>
+          <div >
+            <h4 className="font-bold">Estados</h4>
             <label className="cursor-pointer label">
             <input type="checkbox" name="radio-3" className="radio radio-secondary checkbox" />
-            {/* <h4>Tod@s</h4> */}
+            <span>Tod@s</span>
             </label>
             <label className="cursor-pointer label">
             <input type="checkbox" name="radio-3" className="radio radio-secondary checkbox" />
+            <span>Contratad@s</span>
             </label>
             <label className="cursor-pointer label">
             <input type="checkbox" name="radio-3" className="radio radio-secondary checkbox" />
+            <span>Con entrevistas</span>
             </label>
           </div>
     </div>

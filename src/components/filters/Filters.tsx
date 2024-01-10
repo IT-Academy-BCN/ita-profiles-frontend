f44d34c32648980fce22bae1efe527f0c06aca0b
LUIS VICENTE <bcnluisvi@gmail.com>
2024-01-10 14:15:04 +0100

    improve filters

--- src/components/filters/Filters.tsx
+++ src/components/filters/Filters.tsx
@@ -3,83 +3,59 @@ const Filters = () => {
     <div>
       <h3 className="w-40 text-2xl font-bold text-colortext">Filtros</h3>
       <div>
-        <h4 className="font-bold">Roles</h4>
-      <label className="cursor-pointer label">
-            <input
-              type="checkbox"
-              className="checkbox"
-            />
+        <h4 className="font-bold mt-4 mb-2">Roles</h4>
+        <label className="label cursor-pointer justify-start">
+          <input type="checkbox" className="checkbox mr-2" />
           <span>Frontend</span>
-          </label>
-          <label className="cursor-pointer label">
-            <input
-              type="checkbox"
-              className="checkbox"
-            />
-            <span>Backend</span>
-          </label>
-          <label className="cursor-pointer label">
-            <input
-              type="checkbox"
-              className="checkbox"
-            />
-            <span>Fullstack</span>
-          </label>
-          <label className="cursor-pointer label">
-            <input
-              type="checkbox"
-              className="checkbox"
-            />
-            <span>Data Science</span>
-          </label>
-          </div>
+        </label>
+        <label className="label cursor-pointer justify-start">
+          <input type="checkbox" className="checkbox  mr-2" />
+          <span>Backend</span>
+        </label>
+        <label className="label cursor-pointer justify-start">
+          <input type="checkbox" className="checkbox  mr-2" />
+          <span>Fullstack</span>
+        </label>
+        <label className="label cursor-pointer justify-start">
+          <input type="checkbox" className="checkbox  mr-2" />
+          <span>Data Science</span>
+        </label>
+      </div>
 
-          <div >
-        <h4 className="font-bold">Desarollo</h4>
-      <label className="cursor-pointer label">
-            <input
-              type="checkbox"
-              className="checkbox"
-            />
-            <span>Spring</span>
-          </label>
-          <label className="cursor-pointer label">
-            <input
-              type="checkbox"
-              className="checkbox"
-            />
-            <span>Laravel</span>
-          </label>
-          <label className="cursor-pointer label">
-            <input
-              type="checkbox"
-              className="checkbox"
-            />
-            <span>Angular</span>
-          </label>
-          <label className="cursor-pointer label">
-            <input
-              type="checkbox"
-              className="checkbox"
-            />
-            <span>React</span>
-          </label>
-          </div>
-          <div >
-            <h4 className="font-bold">Estados</h4>
-            <label className="cursor-pointer label">
-            <input type="checkbox" name="radio-3" className="radio radio-secondary checkbox" />
-            <span>Tod@s</span>
-            </label>
-            <label className="cursor-pointer label">
-            <input type="checkbox" name="radio-3" className="radio radio-secondary checkbox" />
-            <span>Contratad@s</span>
-            </label>
-            <label className="cursor-pointer label">
-            <input type="checkbox" name="radio-3" className="radio radio-secondary checkbox" />
-            <span>Con entrevistas</span>
-            </label>
-          </div>
+      <div>
+        <h4 className="font-bold mt-4 mb-2">Desarollo</h4>
+        <label className="label cursor-pointer justify-start">
+          <input type="checkbox" className="checkbox  mr-2" />
+          <span>Spring</span>
+        </label>
+        <label className="label cursor-pointer justify-start">
+          <input type="checkbox" className="checkbox mr-2" />
+          <span>Laravel</span>
+        </label>
+        <label className="label cursor-pointer justify-start">
+          <input type="checkbox" className="checkbox  mr-2" />
+          <span>Angular</span>
+        </label>
+        <label className="label cursor-pointer justify-start">
+          <input type="checkbox" className="checkbox  mr-2" />
+          <span>React</span>
+        </label>
+      </div>
+      <div>
+        <h4 className="font-bold mt-4 mb-2">Estados</h4>
+        <label className="label cursor-pointer justify-start">
+          <input type="checkbox" className="radio-secondary checkbox radio  mr-2" />
+          <span>Tod@s</span>
+        </label>
+        <label className="label cursor-pointer justify-start">
+          <input type="checkbox" className="radio-secondary checkbox radio  mr-2" />
+          <span>Contratad@s</span>
+        </label>
+        <label className="label cursor-pointer justify-start">
+          <input type="checkbox" className="radio-secondary checkbox radio  mr-2" />
+          <span>Entrevistad@s</span>
+        </label>
+      </div>
     </div>
   );
 };

ac11bbf37b62459469418f90646afbb5a27a452c
Dar-ya218 <dasharoura13465@gmail.com>
2024-01-30 11:18:02 +0100

    Made sidemenu

--- src/components/sideNavbar/MenuNavbar.tsx
+++ src/components/sideNavbar/MenuNavbar.tsx
@@ -1,18 +1,26 @@
+import React, { useState } from 'react';
 import itAcademyLogo from '../../assets/svg/itacademy.svg';
 
 const MenuNavbar = () => {
+  const [activeItem, setActiveItem] = useState('Perfiles');
+
+  const menuItems = ['Perfiles', 'Matching', 'Proyectos', 'Estadísticas', 'Guías útiles'];
+
   return (
     <nav className="hidden w-64 items-center p-2 md:block">
       <div className="my-1 flex min-w-[144px] items-center justify-start">
         <img src={itAcademyLogo} alt="itAcademy Logo" className="h-full" />
       </div>
       <div className="mt-24 flex flex-col">
-        {/* Menu should be placed here
-         * Margins or Padding should be placed here
-         * h3 should be removed and component should be placed here maybe
-         * Delete comment once this is fixed
-         */}
-        <h3 className="font-bold text-colortext">MenuNavbar ul</h3>
+      {menuItems.map((item) => (
+          <div
+            key={item}
+            onClick={() => setActiveItem(item)}
+            className={`p-2 ${activeItem === item ? 'text-black border-l-4 border-primary' : 'text-gray-500'}`}
+          >
+            {item}
+          </div>
+        ))}
       </div>
     </nav>
   );

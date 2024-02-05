74a4f94ea70f1336421a091f81f5d041dfe142bb
llopfilms <llopfilms@gmail.com>
2024-02-06 00:32:03 +0100

    Fix MenuNavBar layout using arbitrary values

--- src/components/mainPage/Landing.tsx
+++ src/components/mainPage/Landing.tsx
@@ -6,18 +6,17 @@ import UserDetails from '../userDetailsPanel/UserDetails';
 
 const Landing = () => {
   return (
-    <div className="grid min-h-screen grid-cols-8">
+    <div className="flex min-h-screen">
       <MenuNavbar />
-
-      <div className="col-span-8 w-full md:col-span-7">
+      <div className="flex-1">
         <div className="w-full">
-          <div className=" flex justify-end ">
+          <div className="flex justify-end ">
             <UserNavbar />
           </div>
 
           <main className="flex">
             {/* White board --> */}
-            <div className="w-full rounded-[15px] bg-white p-6  md:mr-8">
+            <div className="w-full rounded-[15px] bg-white p-6 md:mr-8">
               <div className="relative flex gap-1 ">
                 <Filters />
                 <Students />

4f14d7c96072c70e9ea640fbe8597c07a74d7058
Dar-ya218 <dasharoura13465@gmail.com>
2024-02-15 00:50:56 +0100

    Api conected

--- src/components/filters/FiltersContent.tsx
+++ src/components/filters/FiltersContent.tsx
@@ -2,83 +2,34 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const FiltersContent = () => {
-  const [roles, setRoles] = useState([]);
+  const [students, setStudents] = useState([]);
 
   useEffect(() => {
     axios.get("https://itaperfils.eurecatacademy.org/api/v1/student/list/for-home").then((response) => {
-      setRoles(response.data);
+      setStudents(response.data);
     })
     .catch((error) => {
       console.log(error);
     });
   }, []);
+
+  // Extraer todos los roles de todos los estudiantes
+  const roles = students.flatMap(student => student.tags.map(tag => tag.name));
+
   return (
     <>
       <h3 className="mb-7 w-40 text-2xl font-bold md:mb-14">Filtros</h3>
       <div>
         <h4 className="mb-2 font-bold">Roles</h4>
-        {roles.map((roles, index) => (
-        <label key={index} className="label cursor-pointer justify-start p-1">
-          <input
-            type="checkbox"
-            className="checkbox-primary checkbox mr-2 rounded-md border-2 border-gray-500"
-          />
-          <span>{roles}</span>
-        </label>
+        {roles.map((role, index) => (
+          <label key={index} className="label cursor-pointer justify-start p-1">
+            <input
+              type="checkbox"
+              className="checkbox-primary checkbox mr-2 rounded-md border-2 border-gray-500"
+            />
+            <span>{role}</span>
+          </label>
         ))}
-        <label className="label cursor-pointer justify-start p-1">
-          <input
-            type="checkbox"
-            className="checkbox-primary  checkbox mr-2 rounded-md border-2 border-gray-500"
-          />
-          <span>{roles}</span>
-        </label>
-        <label className="label cursor-pointer justify-start p-1">
-          <input
-            type="checkbox"
-            className="checkbox-primary  checkbox mr-2 rounded-md border-2 border-gray-500"
-          />
-          <span>{roles}</span>
-        </label>
-        <label className="label cursor-pointer justify-start p-1">
-          <input
-            type="checkbox"
-            className="checkbox-primary  checkbox mr-2 rounded-md border-2 border-gray-500"
-          />
-          <span>{roles}</span>
-        </label>
-      </div>
-
-      <div>
-        <h4 className="mb-2 mt-4 font-bold">Desarrollo</h4>
-        <label className="label cursor-pointer justify-start p-1">
-          <input
-            type="checkbox"
-            className="checkbox-primary  checkbox mr-2 rounded-md border-2 border-gray-500"
-          />
-          <span>Spring</span>
-        </label>
-        <label className="label cursor-pointer justify-start p-1">
-          <input
-            type="checkbox"
-            className="checkbox-primary checkbox mr-2 rounded-md border-2 border-gray-500"
-          />
-          <span>Laravel</span>
-        </label>
-        <label className="label cursor-pointer justify-start p-1">
-          <input
-            type="checkbox"
-            className="checkbox-primary  checkbox mr-2 rounded-md border-2 border-gray-500"
-          />
-          <span>Angular</span>
-        </label>
-        <label className="label cursor-pointer justify-start p-1">
-          <input
-            type="checkbox"
-            className="checkbox-primary  checkbox mr-2 rounded-md border-2 border-gray-500"
-          />
-          <span>React</span>
-        </label>
       </div>
     </>
   );

46a9fe252b56038bf30621a41407fbdf865095cc
llopfilms <llopfilms@gmail.com>
2024-02-23 00:32:29 +0100

    Set layout for UserDetailedContent inner components

--- src/components/userDetailsPanel/buttons/Buttons.tsx
+++ src/components/userDetailsPanel/buttons/Buttons.tsx
@@ -5,11 +5,11 @@ const Buttons = () => {
   return (
     // not good. Make a single component with just 1 button that through props pass the img svg  and allows to change classnames outside as props.
     <>
-      <button className="h-16 w-16 rounded-xl border-2 border-[#808080]">
-        <img src={bookmark} alt="bookmark icon" className="h-6 w-16" />
+      <button className="h-12 w-12 rounded-2xl border-2 border-gray">
+        <img src={bookmark} alt="bookmark icon" className="h-5 w-16" />
       </button>
-      <button className="h-16 w-16  rounded-xl bg-pinkit hover:bg-pinkit_hover active:bg-pinkit_active">
-        <img src={email} alt="email icon" className="h-6 w-16" />
+      <button className="h-12 w-12  rounded-2xl bg-primary hover:bg-primary-hover active:bg-primary-active">
+        <img src={email} alt="email icon" className="h-5 w-16" />
       </button>
     </>
   );

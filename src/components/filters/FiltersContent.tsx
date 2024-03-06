659a84760e452a524e0090ef7e53bdac41341728
llopFilms <llopFilms@gmail.com>
2024-03-06 14:03:33 +0100

    Create and use specialities & development endpoints

--- src/components/filters/FiltersContent.tsx
+++ src/components/filters/FiltersContent.tsx
@@ -2,57 +2,55 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 const FiltersContent: React.FC = () => {
-  const [specialities, setSpecialities] = useState([]);
+  const [specialities, setSpecialities] = useState<string[]>([]);
+  const [tags, setTags] = useState<string[]>([]);
 
-  const url: string =
+  const urlSpecialities: string =
     'https://itaperfils.eurecatacademy.org/api/v1/specialization/list';
+  const urlTags: string =
+    'https://itaperfils.eurecatacademy.org/api/v1/development/list';
 
-  useEffect(() => {
+  const fetchData = (
+    url: string,
+    setData: React.Dispatch<React.SetStateAction<string[]>>,
+  ) => {
     axios
       .get(url)
       .then((response) => {
-        setSpecialities(response.data);
+        setData(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
-  }, [url]);
+  };
+
+  useEffect(() => {
+    fetchData(urlSpecialities, setSpecialities);
+  }, [urlSpecialities, setSpecialities]);
+
+  useEffect(() => {
+    fetchData(urlTags, setTags);
+  }, [urlTags, setTags]);
 
   return (
     <>
       <h3 className="mb-7 w-40 text-2xl font-bold md:mb-14">Filtros</h3>
       <div>
-        <h4 className="mb-2 font-bold">Roles</h4>
-        <label className="label cursor-pointer justify-start p-1">
-          <input
-            type="checkbox"
-            className="checkbox-primary checkbox mr-2 rounded-md border-2 border-gray-500"
-          />
-          <span>Frontend</span>
-        </label>
-        <label className="label cursor-pointer justify-start p-1">
-          <input
-            type="checkbox"
-            className="checkbox-primary  checkbox mr-2 rounded-md border-2 border-gray-500"
-          />
-          <span>Backend</span>
-        </label>
-        <label className="label cursor-pointer justify-start p-1">
-          <input
-            type="checkbox"
-            className="checkbox-primary  checkbox mr-2 rounded-md border-2 border-gray-500"
-          />
-          <span>Fullstack</span>
-        </label>
-        <label className="label cursor-pointer justify-start p-1">
-          <input
-            type="checkbox"
-            className="checkbox-primary  checkbox mr-2 rounded-md border-2 border-gray-500"
-          />
-          <span>Data Science</span>
-        </label>
+        <h4 className="mb-2 mt-4 font-bold">Roles</h4>
+        {tags &&
+          tags.map((tag, index) => (
+            <label
+              key={index}
+              className="label cursor-pointer justify-start p-1"
+            >
+              <input
+                type="checkbox"
+                className="checkbox-primary  checkbox mr-2 rounded-md border-2 border-gray-500"
+              />
+              <span>{tag}</span>
+            </label>
+          ))}
       </div>
-
       <div>
         <h4 className="mb-2 mt-4 font-bold">Desarrollo</h4>
         {specialities &&

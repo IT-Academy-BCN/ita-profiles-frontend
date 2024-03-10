603f6128235d2a1b939187c4bffc015ce51b563a
Dar-ya218 <dasharoura13465@gmail.com>
2024-03-10 20:04:54 +0100

    small changes duplicate code

--- src/components/mainPage/UserNavbar.tsx
+++ src/components/mainPage/UserNavbar.tsx
@@ -100,35 +100,6 @@ const UserNavbar = () => {
           </div>
         </div>
       )}
-      {isRestrictedPopupOpen && (
-        <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50">
-          <div className="relative flex w-72 flex-col items-center rounded-lg bg-white p-4">
-            <button
-              className="absolute right-2 top-2 h-8 w-8 cursor-pointer rounded-full border-none bg-transparent"
-              onClick={handleCloseRestrictedPopup}
-            >
-              ✕
-            </button>
-            <img src={Lock} alt="Lock" className="mb-4 h-24 w-24" />
-            <h2 className="mb-2 text-lg font-bold">Acceso restringido</h2>
-            <p className="mb-6 ">Entra o regístrate para acceder al perfil</p>
-            <div className="w-full">
-              <button
-                className="mb-2 h-12 w-full rounded-lg bg-primary text-lg text-white"
-                onClick={handleOpenRegisterPopup}
-              >
-                Soy candidato/a
-              </button>
-              <button
-                className="h-12 w-full rounded-lg bg-primary text-lg text-white"
-                onClick={handleOpenLoginPopup}
-              >
-                ¿Tienes cuenta?
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
       {isRegisterPopupOpen && (
         <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50">
           <RegisterPopup

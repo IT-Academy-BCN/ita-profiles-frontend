86a2922a9c5d7bf9f8c187574a690c348c739cf5
Dar-ya218 <dasharoura13465@gmail.com>
2024-03-11 22:39:59 +0100

    clean cod

--- src/components/Bootcamp.tsx
+++ src/components/Bootcamp.tsx
@@ -3,7 +3,7 @@ const Bootcamp = () => {
   return (
     <div>
        <h1 className="text-start font-bold mb-5 mt-8 text-lg">Datos del bootcamp</h1>
-      <div className="relative bg-bootcamp w-475 flex items-center rounded-md shadow-custom">
+      <div className="relative bg-bootcamp flex items-center rounded-md shadow-custom">
         <img src={medal} alt="Medal" className="w-66 h-66 ml-4 mt-6 mb-6 mr-1.25" />
         <div className="h-[63px]">
           <h2 className="font-poppins text-xl font-semibold leading-tight text-left text-black">Full-stack PHP</h2>

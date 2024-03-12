57b000e1c6ccee3ece479f860883a8f5be2c1710
Dar-ya218 <dasharoura13465@gmail.com>
2024-03-12 01:12:56 +0100

    lenguages component done

--- src/components/userDetailsPanel/UserDetailsContent.tsx
+++ src/components/userDetailsPanel/UserDetailsContent.tsx
@@ -5,6 +5,7 @@ import type { TSmallScreenContext } from '../../interfaces/interfaces';
 import Projects from './projects/ProjectsCard';
 import Collaboration from './collaborationSection/Collaboration';
 import Bootcamp from './bootcampSection/BootcampCard';
+import LenguagesCard from './lenguagesSection/LenguagesCard';
 
 type TUserDetailsContent = {
   handleIsPanelOpen: () => void;
@@ -40,9 +41,7 @@ const UserDetailsContent: React.FC<TUserDetailsContent> = ({
           </div>
           <Collaboration />
           <Bootcamp />
-          <div className="border-gray-300 h-[100px] rounded-xl border-2 p-2">
-            Languages component
-          </div>
+          <LenguagesCard />
           <div className="border-gray-300 h-[70px] rounded-xl border-2 p-2">
             Modality component
           </div>
---
+++ src/components/userDetailsPanel/lenguagesSection/LenguagesCard.tsx
@@ -0,0 +1,13 @@
+const LenguagesCard:React.FC= () => {
+  return (
+	<div>
+	   <h1 className="font-bold pb-5 pt-8 text-lg">Idiomas</h1>
+		<div className="flex flex-col">
+		  <h2 className="text-sm font-semibold leading-tight text-black">Inglés · Intermedio</h2>
+		  <h2 className="text-sm font-semibold leading-tight text-black pt-4">Francés · Básico</h2>
+		</div>
+	  </div>
+  );
+}
+
+export default LenguagesCard;

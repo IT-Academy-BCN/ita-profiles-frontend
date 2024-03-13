3bc6b5b7500365b2c7ca5197f6d0489359024e1c
leidyz <leidy.zhang98@gmail.com>
2024-03-13 18:00:48 +0100

    include modality card

--- src/components/studentDetail/StudentDetailsContent.tsx
+++ src/components/studentDetail/StudentDetailsContent.tsx
@@ -5,6 +5,7 @@ import type { TSmallScreenContext } from '../../interfaces/interfaces';
 import ProjectsCard from '../studentDetailCards/projectsSection/ProjectsCard';
 import CollaborationCard from '../studentDetailCards/collaborationSection/CollaborationCard';
 import BootcampCard from '../studentDetailCards/bootcampSection/BootcampCard';
+import ModalityCard from '../studentDetailCards/modalitySection/modalityCard';
 
 type TStudentDetailsContent = {
   handleIsPanelOpen: () => void;
@@ -17,9 +18,8 @@ const StudentDetailsContent: React.FC<TStudentDetailsContent> = ({
 
   return (
     <div
-      className={`flex flex-col gap-6 ${
-        isMobile ? 'modal-box rounded-2xl p-2 pl-4' : 'h-full'
-      }`}
+      className={`flex flex-col gap-6 ${isMobile ? 'modal-box rounded-2xl p-2 pl-4' : 'h-full'
+        }`}
     >
       <div className="flex items-center justify-end p-3 md:justify-between">
         <h3 className="hidden text-2xl font-bold md:block">Detalle Perfil</h3>
@@ -36,9 +36,10 @@ const StudentDetailsContent: React.FC<TStudentDetailsContent> = ({
           <ProjectsCard />
           <CollaborationCard />
           <BootcampCard />
+          <ModalityCard />
           <div className="border-gray-300 h-[100px] rounded-xl border-2 p-2">
             Other Education
-          </div>
+            M</div>
           <div className="border-gray-300 h-[100px] rounded-xl border-2 p-2">
             Languages component
           </div>

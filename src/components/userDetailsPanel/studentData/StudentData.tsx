813c2740a6b489ed4ad6283fbbd8fe0ff8170f4c
Stefis0702 <stefis0528@gmail.com>
2024-03-13 12:04:32 +0100

    layout-2

--- src/components/userDetailsPanel/studentData/StudentData.tsx
+++ src/components/userDetailsPanel/studentData/StudentData.tsx
@@ -1,11 +1,10 @@
 import profilePicture from '../../../assets/img/stud_1.png';
 import bookmark from '../../../assets/svg/bookmark.svg';
-import email from '../../../assets/svg/email.svg';
+import email from '../../../assets/img/mail-gray.png';
 import github from '../../../assets/svg/github.svg';
 import linkedin from '../../../assets/svg/linkedin.svg';
 
 const StudentData = () => {
-
   const studentsData = [
     {
       id: 1,
@@ -14,50 +13,89 @@ const StudentData = () => {
         subtitle: 'Full-stack developer PHP',
         socialMedia: {
           linkedin: {
-            url: 'GitHub',
+            url: 'LinkedIn',
           },
           github: {
-            url: 'LinkedIn',
-          }
+            url: 'Github',
+          },
         },
         about: {
-          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, voluptatum iste. Commodi, libero adipisci. Dignissimos consequuntur ab excepturi incidunt ducimus!",
+          description:
+            'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, voluptatum iste. Commodi, libero adipisci. Dignissimos consequuntur ab excepturi incidunt ducimus!',
         },
-        tags: ['PHP', 'Laravel', 'SQL', 'MongoDB', 'Javascript', 'React', 'Redux', 'Github', 'CI/CD', 'TDD']
+        tags: [
+          'PHP',
+          'Laravel',
+          'SQL',
+          'MongoDB',
+          'Javascript',
+          'React',
+          'Redux',
+          'Github',
+          'CI/CD',
+          'TDD',
+        ],
       },
-    }
-
-  ]
-
+    },
+  ];
 
   return (
     <div>
-      <h3 className="text-2xl font-bold text-black-3">Detalles Perfil</h3>
+     
       {studentsData.map((student) => (
-        <div key={student.id} className='flex flex-col gap-4'>
-          <div className='flex'>
-            <img src={profilePicture} alt="Profile picture" className='rounded-lg' />
-            <div className='flex'>
-              <div>
-                <h1 className='text-xl font-bold'>{student.profileDetail.fullname}</h1>
-                <h2 className='font-semibold text-gray-2'>{student.profileDetail.subtitle}</h2>
-              </div>
-              <div className='border-2 flex'>
-                <img src={bookmark} alt="bookmark icon" />
-                <img src={email} alt="email icon" />
+        <div key={student.id} className="flex flex-col gap-4">
+          <div className="flex gap-3 mb-3">
+            <img
+              src={profilePicture}
+              alt="Profile picture"
+              className="w-32 rounded-lg"
+            />
+            <div className="flex w-full justify-between ">
+              <div className="flex flex-col justify-between w-full">
+                <div className="flex justify-between w-full ">
+                  <div className="flex flex-col ">
+                    <h1 className="text-xl font-bold">
+                      {student.profileDetail.fullname}
+                    </h1>
+                    <h2 className=" text-gray-2">
+                      {student.profileDetail.subtitle}
+                    </h2>
+                  </div>
+
+                  <div className=" hidden items-center gap-2  lg:flex ">
+                    <img
+                      src={bookmark}
+                      alt="bookmark icon"
+                      className="h-6 w-6"
+                    />
+                    <img src={email} alt="email icon" className="h-6 w-6" />
+                  </div>
+                </div>
+
+                <div className="flex gap-4 ">
+                  <div className="flex gap-1">
+                    <img src={github} alt="github icon" />
+                    {student.profileDetail.socialMedia.github.url}
+                  </div>
+                  <div className="flex gap-1">
+                    <img src={linkedin} alt="linkedin icon" />
+                    {student.profileDetail.socialMedia.linkedin.url}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div className='flex gap-2'>
-            <div className='flex gap-1'><img src={github} alt="" />{student.profileDetail.socialMedia.github.url}</div>
-            <div className='flex gap-1'><img src={linkedin} alt="" />{student.profileDetail.socialMedia.linkedin.url}</div>
-          </div>
-
-          <h2 className='text-md font-bold'>About</h2>
+          
+          <h2 className="text-md font-bold">About</h2>
           <p>{student.profileDetail.about.description}</p>
-          <ul className='flex flex-wrap gap-1'>
+          <ul className="flex flex-wrap gap-1">
             {student.profileDetail.tags.map((tag, index) => (
-              <li key={index} className='rounded-md bg-gray-4-base px-2 py-1 text-sm'>{tag}</li>
+              <li
+                key={index}
+                className="rounded-md bg-gray-4-base px-2 py-1 text-sm"
+              >
+                {tag}
+              </li>
             ))}
           </ul>
         </div>

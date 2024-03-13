c3017db8470f483e5bfc0f7ef203ddd3585190a6
Dar-ya218 <dasharoura13465@gmail.com>
2024-03-13 22:56:33 +0100

    finish

--- src/components/userDetailsPanel/lenguagesSection/LenguagesCard.tsx
+++ src/components/userDetailsPanel/lenguagesSection/LenguagesCard.tsx
@@ -4,7 +4,7 @@ const LenguagesCard:React.FC= () => {
 	   <h1 className="font-bold pb-6 pt-8 text-lg">Idiomas</h1>
 		<div className="flex flex-col">
 		  <h2 className="text-sm font-semibold leading-tight text-black">Inglés · Intermedio</h2>
-		  <h2 className="text-sm font-semibold leading-tight text-black pt-5">Francés · Básico</h2>
+		  <h2 className="text-sm font-semibold leading-tight text-black pt-4">Francés · Básico</h2>
 		</div>
 	  </div>
   );

e8bb1e9557a4a0bb2b71045feca9c8c04508a8f8
Dar-ya218 <dasharoura13465@gmail.com>
2024-03-14 22:54:23 +0100

    Give format

--- src/components/userDetailsPanel/lenguageSection/LenguageCard.tsx
+++ src/components/userDetailsPanel/lenguageSection/LenguageCard.tsx
@@ -1,13 +1,17 @@
-const LenguageCard:React.FC= () => {
+const LenguageCard: React.FC = () => {
   return (
-	<div>
-	   <h1 className="font-bold pb-6 pt-8 text-lg">Idiomas</h1>
-		<div className="flex flex-col">
-		  <h2 className="text-sm font-semibold leading-tight text-black">Inglés · Intermedio</h2>
-		  <h2 className="text-sm font-semibold leading-tight text-black pt-4">Francés · Básico</h2>
-		</div>
-	  </div>
+    <div>
+      <h1 className="pb-6 pt-8 text-lg font-bold">Idiomas</h1>
+      <div className="flex flex-col">
+        <h2 className="text-sm font-semibold leading-tight text-black">
+          Inglés · Intermedio
+        </h2>
+        <h2 className="pt-4 text-sm font-semibold leading-tight text-black">
+          Francés · Básico
+        </h2>
+      </div>
+    </div>
   );
-}
+};
 
 export default LenguageCard;

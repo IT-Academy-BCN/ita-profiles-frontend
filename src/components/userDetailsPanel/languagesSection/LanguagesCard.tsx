a5148615f6e0fdf192e1d04a075b4fbd50082da4
Dar-ya218 <dasharoura13465@gmail.com>
2024-03-17 21:57:55 +0100

    change

--- src/components/userDetailsPanel/languagesSection/LanguagesCard.tsx
+++ src/components/userDetailsPanel/languagesSection/LanguagesCard.tsx
@@ -3,10 +3,10 @@ const LanguagesCard: React.FC = () => {
     <div className="flex flex-col gap-8">
       <h3 className="text-lg font-bold">Idiomas</h3>
       <div className="flex flex-col gap-6">
-        <p className="text-sm font-semibold leading-tight text-black">
+        <p className="text-sm font-semibold text-black">
           Inglés · Intermedio
         </p>
-        <p className="text-sm font-semibold leading-tight text-black">
+        <p className="text-sm font-semibold text-black">
           Francés · Básico
         </p>
       </div>

480bbc43d2d9d750eb40058e2a47760756142fa4
Dar-ya218 <dasharoura13465@gmail.com>
2024-03-17 22:09:19 +0100

    px

--- src/components/userDetailsPanel/languagesSection/LanguagesCard.tsx
+++ src/components/userDetailsPanel/languagesSection/LanguagesCard.tsx
@@ -1,8 +1,8 @@
 const LanguagesCard: React.FC = () => {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-6">
       <h3 className="text-lg font-bold">Idiomas</h3>
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-5">
         <p className="text-sm font-semibold text-black">
           Inglés · Intermedio
         </p>

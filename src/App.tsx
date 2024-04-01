903b6f4131ce7c3ff587d578adf251be97fbf8f9
Dar-ya218 <dasharoura13465@gmail.com>
2024-04-01 19:53:50 +0200

    install Redux and add to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { Provider } from 'react-redux';
+import store from './store';
+import type { Store } from 'redux'; 
 import SmallScreenProvider from './context/SmallScreenContext';
 import Home from './pages/Home';
 import './styles/App.css';
 
 const App = () => {
   return (
-    <SmallScreenProvider>
-      <Home />
-    </SmallScreenProvider>
+    <Provider store={store as Store}> // Specify the type for the store
+      <SmallScreenProvider>
+        <Home />
+      </SmallScreenProvider>
+    </Provider> 
   );
 };
 

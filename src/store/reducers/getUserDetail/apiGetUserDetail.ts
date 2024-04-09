3df2c2919aa6bbf11f06c743dd996f19c91e1ad9
Dar-ya218 <dasharoura13465@gmail.com>
2024-04-09 23:00:48 +0200

    add rules

--- src/store/reducers/getUserDetail/apiGetUserDetail.ts
+++ src/store/reducers/getUserDetail/apiGetUserDetail.ts
@@ -1,17 +1,20 @@
-
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 // import { PayloadAction } from "@reduxjs/toolkit";
 
 const initialState = {
     isUserPanelOpen: false,
+    filteredStudents: ''
 }
 
 const showUserInfo = createSlice({
     name: "showUserReducer",
     initialState,
     reducers: {
-        toggleUserPanel: (state) => {state.isUserPanelOpen = !state.isUserPanelOpen}
+        toggleUserPanel: (state) => {state.isUserPanelOpen = !state.isUserPanelOpen},
+        setFilteredStudents: (state, action: PayloadAction<string>) => {
+            state.filteredStudents = action.payload;
+        }  
     }
 })
-export const { toggleUserPanel } = showUserInfo.actions;
+export const { toggleUserPanel, setFilteredStudents  } = showUserInfo.actions;
 export default showUserInfo.reducer

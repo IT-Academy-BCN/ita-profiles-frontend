c7a43fd84e8aedbb38204c60e7947c3923c1f765
Fabrizio Contu <fab.contu@gmail>
2024-04-11 14:26:00 +0200

    ContextProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,18 @@ import { I18nextProvider } from 'react-i18next'
 import App from './App'
 import i18n from './locales/i18n'
 import { store } from './store/store'
-
 import './styles/normalize.css'
 import './styles/index.css'
+import { ContextProvider } from './context/Context'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    </Provider>
+    <ContextProvider>
+      <Provider store={store}>
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
+      </Provider>
+    </ContextProvider>
   </React.StrictMode>,
 )

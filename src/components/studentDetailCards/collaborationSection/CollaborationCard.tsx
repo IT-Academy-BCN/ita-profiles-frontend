60dd5bfb36e3dac597ca8f4b1843a4c909f407f5
Jordi Morillo <jmdesarrollo82@gmail.com>
2024-04-11 16:36:25 +0200

    feature: add tests for StudentDetailsContent

--- src/components/studentDetailCards/collaborationSection/CollaborationCard.tsx
+++ src/components/studentDetailCards/collaborationSection/CollaborationCard.tsx
@@ -2,7 +2,7 @@ import ChallengesCard from './ChallengesCard'
 import ResourcesCard from './ResourcesCard'
 
 const CollaborationCard = () => (
-  <div className="flex flex-col gap-4">
+  <div className="flex flex-col gap-4" data-testid="CollaborationCard">
     <h3 className="text-lg font-bold text-black-3">Colaboración</h3>
     <div className="flex flex-col gap-4 md:flex-row">
       <ResourcesCard />

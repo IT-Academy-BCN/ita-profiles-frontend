34ed293476ceb646b291df16e7327326aa88a2ab
Dar-ya218 <dasharoura13465@gmail.com>
2024-04-14 23:25:42 +0200

    fix FetchStudentsList

--- src/api/FetchStudentsList.ts
+++ src/api/FetchStudentsList.ts
@@ -2,13 +2,11 @@ import axios from "axios";
 import { IStudentList } from "../interfaces/interfaces";
 
 export const FetchStudentsListHome = async (students: string) => {
-  console.log('STUDENTSSS',students);
     try {
-     // const response = await axios.get<IStudentList[]>("https://itaperfils.eurecatacademy.org/api/v1/student/list/for-home");
-     const response = await axios.get<IStudentList[]>("https://itaperfils.eurecatacademy.org/api/v1/student/list/for-home?specialization="+students);
+     const response = await axios.get<IStudentList[]>(`https://itaperfils.eurecatacademy.org/api/v1/student/list/for-home?specialization=${students}`);
       return response.data
-
     } catch (e) {
-      console.error(e);
+      //console.error(e);
+      return null;
     }
   };

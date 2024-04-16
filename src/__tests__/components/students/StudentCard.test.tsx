e987e8f3240df4d84c971ba5b0da745c4338119c
Luiggi <luduve97@gmail.com>
2024-04-16 18:18:29 +0200

    fix test issues causing pr to fail 1 - remove provider

--- src/__tests__/components/students/StudentCard.test.tsx
+++ src/__tests__/components/students/StudentCard.test.tsx
@@ -1,13 +1,7 @@
 import { render } from '@testing-library/react'
-import { Provider } from 'react-redux'
 import StudentCard from '../../../components/students/StudentCard'
 import { IStudentList } from '../../../interfaces/interfaces'
-import { store } from '../../../store/store'
 
-// We dont need to import anything like describe, it etc from vitest
-// We also dont need to import anything from react-testing-library
-// Kevin already setted it up so it doesnt become repetitive and you
-// Just do the tests...
 const mockStudentCard: IStudentList = {
   id: 'abc123',
   fullname: 'John',
@@ -20,11 +14,7 @@ const mockStudentCard: IStudentList = {
 }
 describe('StudentCard', () => {
   it('should render the student card component', () => {
-    const { container } = render(
-      <Provider store={store}>
-        <StudentCard {...mockStudentCard} />
-      </Provider>,
-    )
+    const { container } = render(<StudentCard {...mockStudentCard} />)
     expect(container).toBeInTheDocument()
   })
 })

43caaa8e636d054bba9078632355c9e12e07c609
llopfilms <llopfilms@gmail.com>
2024-04-17 21:41:44 +0200

    Update Tests

--- src/__tests__/components/studentDetailCards/LanguageCard.test.tsx
+++ src/__tests__/components/studentDetailCards/LanguageCard.test.tsx
@@ -32,4 +32,23 @@ describe('LanguagesCard', () => {
     expect(mockLanguages.length).toBe(3)
     expect(screen.getByText('Idiomas')).toBeInTheDocument()
   })
+
+  it('check the map aover mockLanguages', () => {
+    render(
+      <div className="flex flex-col gap-1">
+        {mockLanguages &&
+          mockLanguages.map((mockLanguage) => (
+            <ul key={mockLanguage.language_id} className="flex flex-col">
+              <li className="text-sm font-semibold text-black-2">
+                {mockLanguage.language_name}
+              </li>
+            </ul>
+          ))}
+      </div>,
+    )
+
+    expect(screen.getByText('Español')).toBeInTheDocument()
+    expect(screen.getByText('Català')).toBeInTheDocument()
+    expect(screen.getByText('English')).toBeInTheDocument()
+  })
 })

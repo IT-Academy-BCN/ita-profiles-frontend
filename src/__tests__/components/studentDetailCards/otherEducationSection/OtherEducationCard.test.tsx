d40e8c47e101654f57196215db992967145b25bd
Luiggi <luduve97@gmail.com>
2024-04-23 19:49:10 +0200

    add mock adapter in setup + testing test

--- src/__tests__/components/studentDetailCards/otherEducationSection/OtherEducationCard.test.tsx
+++ src/__tests__/components/studentDetailCards/otherEducationSection/OtherEducationCard.test.tsx
@@ -1,9 +1,24 @@
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import OtherEducationCard from '../../../../components/studentDetailCards/otherEducationSection/OtherEducationCard'
+import { mock } from '../../../setup'
 
 describe('OtherEducationCard', () => {
   it('should render OtherEducationCard component correctly', () => {
     render(<OtherEducationCard />)
+  })
+  it('should fetch the student Additional Training correctly', () => {
+    mock.onGet('/additionaltraining').reply(200, {
+      additional_trainings: [
+        {
+          uuid: '123',
+          course_name: 'React Bootcamp',
+          study_center: 'IT academy',
+          course_beginning_year: 2023,
+          course_ending_year: 2024,
+          duration_hrs: 100,
+        },
+      ],
+    })
   })
 })
--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -2,16 +2,18 @@
 import { expect, afterEach, beforeAll } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import matchers from '@testing-library/jest-dom/matchers'
+import MockAdapter from 'axios-mock-adapter'
+import axios from 'axios'
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers)
 
-// Establish API mocking before all tests.
-beforeAll(() => {})
+export const mock = new MockAdapter(axios, { onNoMatch: 'throwException' })
 
-// Reset any request handlers that we may add during the tests,
-// so they don't affect other tests.
+beforeAll(() => {
+  mock.reset()
+})
+
 afterEach(() => {
   cleanup()
 })
-// Clean up after the tests are finished.

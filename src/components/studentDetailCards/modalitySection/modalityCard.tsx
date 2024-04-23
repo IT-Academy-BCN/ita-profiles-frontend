2257d83e08d7ae8302062970523d58c9ad607e07
Fabrizio Contu <fab.contu@gmail>
2024-04-23 13:21:42 +0200

    update

--- src/components/studentDetailCards/modalitySection/modalityCard.tsx
+++ src/components/studentDetailCards/modalitySection/modalityCard.tsx
@@ -1,26 +1,22 @@
 import { useEffect, useState } from 'react'
 import remoto from '../../../assets/svg/remoto.svg'
 import { useStudentIdContext } from '../../../context/StudentIdContext'
-import type { TModality } from '../../../interfaces/interfaces'
 import axios from 'axios'
+import type { TModality } from '../../../interfaces/interfaces'
 
 const ModalityCard: React.FC = () => {
   const { studentUUID } = useStudentIdContext()
-  const [modalityData, setModalityData] = useState<TModality[]>([])
-  console.log(studentUUID)
+  const [modalityData, setModalityData] = useState<TModality>({ modality: [] })
 
   const endpointModality = `https://itaperfils.eurecatacademy.org/api/v1/modality/${studentUUID}`
 
   useEffect(() => {
     if (studentUUID) {
       axios
-        .get(endpointModality, {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
+        .get<TModality>(endpointModality)
         .then((response) => {
-          setModalityData(response.data.languages)
+          setModalityData(response.data)
+          console.log(modalityData)
         })
         .catch((error) => {
           console.error(error.message)
@@ -33,16 +29,14 @@ const ModalityCard: React.FC = () => {
       <h3 className="font-bold text-lg">Modalidad</h3>
       <div className="flex items-center gap-2">
         <img src={remoto} className="pr-2" alt="remoto" />
-        {modalityData &&
-          modalityData.map((modality, index) => (
-            <ul key={studentUUID} className="flex flex-col">
-              <li className="text-sm font-semibold text-black-2">
-                {modality.modality[index]}
-              </li>
-            </ul>
-          ))}
+        {modalityData.modality.map((modality, index) => (
+          <p key={index} className="flex flex-col">
+            {modality}
+          </p>
+        ))}
       </div>
     </div>
   )
 }
+
 export default ModalityCard

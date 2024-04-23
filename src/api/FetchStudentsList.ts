789cadaa5b40f1df1526e9c822bbce21542af270
Jordi Morillo <jmdesarrollo82@gmail.com>
2024-04-23 13:14:25 +0200

    feature: add tests for FetchStudentsList api query

--- src/api/FetchStudentsList.ts
+++ src/api/FetchStudentsList.ts
@@ -1,29 +1,31 @@
-import axios from 'axios'
-import { IStudentList } from '../interfaces/interfaces'
-import { List } from 'postcss/lib/list';
+// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+import axios, { AxiosError } from 'axios';
+import { IStudentList } from '../interfaces/interfaces';
 
 // eslint-disable-next-line consistent-return
-export const FetchStudentsListHome = async (selectedRoles:Array, selectedTags:Array) => {
-  try {
-    let queryParams = '';
+  export const FetchStudentsListHome = async (selectedRoles:Array<string>, selectedTags:Array<string>) => {
 
-    // Construir la cadena de consulta para roles seleccionados
-    if (selectedRoles.length > 0) {
-      queryParams += `specialization=${selectedRoles.join(',')}`;
+    try {
+      let queryParams = '';
+
+      // Construir la cadena de consulta para roles seleccionados
+      if (selectedRoles.length > 0) {
+        queryParams += `specialization=${selectedRoles.join(',')}`;
+      }
+
+      // Añadir tags a la cadena de consulta si están presentes
+      if (selectedTags.length > 0) {
+        queryParams += `${queryParams ? '&' : ''}tags=${selectedTags.join(',')}`;
+      }
+
+      // Construir la URL completa con la cadena de consulta
+      const url = `https://itaperfils.eurecatacademy.org/api/v1/student/list/for-home${queryParams ? `?${queryParams}` : ''}`;
+
+      const response = await axios.get<IStudentList[]>(url);
+      return response.data;
+      // @ts-expect-error throws AxiosError exception
+    } catch (e: AxiosError) {
+      // eslint-disable-next-line no-console
+      throw new DOMException(e.message, 'ConnectionFailed');
     }
-
-    // Añadir tags a la cadena de consulta si están presentes
-    if (selectedTags.length > 0) {
-      queryParams += `${queryParams ? '&' : ''}tags=${selectedTags.join(',')}`;
-    }
-
-    // Construir la URL completa con la cadena de consulta
-    const url = `https://itaperfils.eurecatacademy.org/api/v1/student/list/for-home${queryParams ? `?${queryParams}` : ''}`;
-
-    const response = await axios.get<IStudentList[]>(url);
-    return response.data;
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e)
   }
-}

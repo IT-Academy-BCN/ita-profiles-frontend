4b4cbab47182640bf6c295989d00f084470882c7
Fabrizio Contu <fab.contu@gmail>
2024-04-24 21:21:47 +0200

    update

--- src/components/studentDetailCards/bootcampSection/BootcampCard.tsx
+++ src/components/studentDetailCards/bootcampSection/BootcampCard.tsx
@@ -1,15 +1,15 @@
-import medal from '../../../assets/img/medal-dynamic-color.png'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
+import medal from '../../../assets/img/medal-dynamic-color.png'
 import { useStudentIdContext } from '../../../context/StudentIdContext'
 import type { TBootcamp } from '../../../interfaces/interfaces'
-import axios from 'axios'
 
 const BootcampCard: React.FC = () => {
   const { studentUUID } = useStudentIdContext()
 
   const [bootcampData, setBootcampData] = useState<TBootcamp[]>([])
 
-  const endpointBootcamp = `https://itaperfils.eurecatacademy.org/api/v1/api/v1/students/${studentUUID}/bootcamp`
+  const endpointBootcamp = `https://itaperfils.eurecatacademy.org/api/v1/students/${studentUUID}/bootcamp`
 
   useEffect(() => {
     if (studentUUID) {
@@ -21,14 +21,14 @@ const BootcampCard: React.FC = () => {
         })
         .then((response) => {
           setBootcampData(response.data.bootcamp)
-          console.log(bootcampData)
         })
         .catch((error) => {
+          // eslint-disable-next-line no-console
           console.error(error.message)
+          throw error
         })
     }
   }, [studentUUID, endpointBootcamp])
-  console.log(studentUUID)
 
   return (
     <div className="flex flex-col gap-4" data-testid="BootcampCard">
@@ -36,11 +36,10 @@ const BootcampCard: React.FC = () => {
       <div className="flex items-center gap-1 rounded-md bg-gray-5-background p-5 shadow-[0_4px_0_0_rgba(0,0,0,0.25)]">
         <img src={medal} alt="Medal" className="w-16" />
         {bootcampData.map((bootcamp) => (
-          <div key={bootcamp.bootcamp_id} className="flex flex-col">
+          <div key={bootcamp.bootcamp_id} className="flex flex-col gap-1">
             <p className="text-xl font-semibold leading-tight text-black-3">
               {bootcamp.bootcamp_name}
             </p>
-
             <p className="text-base font-semibold text-black-2">
               Finalizado: {bootcamp.bootcamp_end_date}
             </p>

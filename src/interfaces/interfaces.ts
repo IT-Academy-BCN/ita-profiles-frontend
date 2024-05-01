d2fbb4677359417f17e929e7f9f925613d2164b6
Luiggi <luduve97@gmail.com>
2024-05-01 17:56:11 +0200

    lgtm

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -45,3 +45,11 @@ export type TLanguage = {
   language_name: string
   language_level: string
 }
+export type TAdditionalTraining = {
+  uuid: string
+  course_name: string
+  study_center: string
+  course_beginning_year: number
+  course_ending_year: number
+  duration_hrs: number
+}

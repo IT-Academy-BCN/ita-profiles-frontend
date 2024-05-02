9cf0f969e342a22ad52a6ae53ba0883dc04c9d18
Fabrizio Contu <fab.contu@gmail>
2024-05-02 13:53:31 +0200

    update bootcampData rendering

--- src/components/studentDetailCards/bootcampSection/BootcampCard.tsx
+++ src/components/studentDetailCards/bootcampSection/BootcampCard.tsx
@@ -22,19 +22,30 @@ const BootcampCard: React.FC = () => {
   return (
     <div className="flex flex-col gap-4" data-testid="BootcampCard">
       <h3 className="text-lg font-bold">Datos del bootcamp</h3>
-      <div className="flex items-center gap-1 rounded-md bg-gray-5-background p-5 shadow-[0_4px_0_0_rgba(0,0,0,0.25)]">
-        <img src={medal} alt="Medal" className="w-16" />
-        {bootcampData.map((bootcamp) => (
-          <div key={bootcamp.bootcamp_id} className="flex flex-col gap-1">
-            <p className="text-xl  font-semibold leading-tight text-black-3">
-              {bootcamp.bootcamp_name}
-            </p>
-            <p className="text-base font-medium text-black-2">
-              Finalizado: {bootcamp.bootcamp_end_date}
-            </p>
+
+      {bootcampData.length === 0 ? (
+        <div className="flex flex-col gap-1 rounded-md bg-gray-5-background p-5 shadow-[0_4px_0_0_rgba(0,0,0,0.25)]">
+          <p className="text- font-medium text-black-3">
+            - Bootcamp no terminado -
+          </p>
+        </div>
+      ) : (
+        bootcampData.map((bootcamp) => (
+          <div className="flex flex-col gap-1 rounded-md bg-gray-5-background p-5 shadow-[0_4px_0_0_rgba(0,0,0,0.25)]">
+            <div className="flex items-center" key={bootcamp.bootcamp_id}>
+              <img src={medal} alt="Medal" className="w-16 pe-1" />
+              <div className="flex flex-col gap-1">
+                <p className="text-base font-semibold leading-tight text-black-3">
+                  {bootcamp.bootcamp_name}
+                </p>
+                <p className="text-sm font-medium text-black-2">
+                  Finalizado: {bootcamp.bootcamp_end_date}
+                </p>
+              </div>
+            </div>
           </div>
-        ))}
-      </div>
+        ))
+      )}
     </div>
   )
 }

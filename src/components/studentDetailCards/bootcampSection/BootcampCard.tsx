dd13953b391717f9a8b83d358c1c75090d2f6606
Fabrizio Contu <fab.contu@gmail>
2024-05-02 10:10:07 +0200

    change from object to array of objects

--- src/components/studentDetailCards/bootcampSection/BootcampCard.tsx
+++ src/components/studentDetailCards/bootcampSection/BootcampCard.tsx
@@ -7,13 +7,13 @@ import { fetchBootcampData } from '../../../api/FetchStudentBootcamp'
 const BootcampCard: React.FC = () => {
   const { studentUUID } = useStudentIdContext()
 
-  const [bootcampData, setBootcampData] = useState<TBootcamp | null>(null)
+  const [bootcampData, setBootcampData] = useState<TBootcamp[]>([])
 
   useEffect(() => {
     const fetchBootcamp = async () => {
       if (studentUUID) {
         const data = await fetchBootcampData(studentUUID)
-        setBootcampData(data)
+        setBootcampData(data.bootcamps)
       }
     }
     fetchBootcamp()
@@ -24,17 +24,16 @@ const BootcampCard: React.FC = () => {
       <h3 className="text-lg font-bold">Datos del bootcamp</h3>
       <div className="flex items-center gap-1 rounded-md bg-gray-5-background p-5 shadow-[0_4px_0_0_rgba(0,0,0,0.25)]">
         <img src={medal} alt="Medal" className="w-16" />
-
-        {bootcampData && (
-          <div key={bootcampData.bootcamp_id} className="flex flex-col gap-1">
+        {bootcampData.map((bootcamp) => (
+          <div key={bootcamp.bootcamp_id} className="flex flex-col gap-1">
             <p className="text-xl  font-semibold leading-tight text-black-3">
-              {bootcampData.bootcamp_name}
+              {bootcamp.bootcamp_name}
             </p>
             <p className="text-base font-medium text-black-2">
-              Finalizado: {bootcampData.bootcamp_end_date}
+              Finalizado: {bootcamp.bootcamp_end_date}
             </p>
           </div>
-        )}
+        ))}
       </div>
     </div>
   )
